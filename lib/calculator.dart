abdb8c71576b31d1dd59af7b90e32d8880946bc3
Ivan Trajanovski <ivan.trajanovski@egroup.hu>
2022-06-02 14:38:17 +0200

    first commit

---
+++ lib/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
